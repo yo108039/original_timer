1127d01ffc39e95b542c46bf8e58cad9cbad9feb
yo108039 <y_obara127@yahoo.co.jp>
2021-06-15 23:40:59 +0900

    ログインユーザーの対象データのみ表示

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,6 +1,6 @@
 class LogsController < ApplicationController
   def index
-    @meetings = Meeting.all
+    @meetings = Meeting.find_by_sql(['select * from meetings where user_id = :id', {id: current_user.id}])
   end
 
 end
